86b6828f515d4798901dbd358937bfb72007db83
fredie <contact@funabia.com>
2021-11-10 22:57:31 +0000

    Add Airflow setup script and readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Airflow POC
+
+Experimenting with airflow by setting up test DAGs with some python scripts.
+
+## Setup
+
+Create a virtual Python environment with `venv`
+
+```sh
+python -m venv name-of-env
+```
+
+and activate it.
+
+```sh
+source name-of-env/bin/activate
+```
+
+Run the setup script for Airflow `setup-airflow.sh` which will:
+
+- Download and install Airflow 2.2.1
+- Initialise a SQLite database
+- Create a user
+- Start a local Airflow server on http://localhost:8080
+
+Proceed to the local instance of Airflow on the browser and log in with the provided user details on the terminal output of the set up script.
---
+++ setup_airflow.sh
@@ -0,0 +1,21 @@
+# Airflow needs a home. `~/airflow` is the default, but you can put it
+# somewhere else if you prefer (optional)
+export AIRFLOW_HOME=~/airflow
+
+# Install Airflow using the constraints file based on the versions of 
+# Python and Airflow
+AIRFLOW_VERSION=2.2.1
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.2.1/constraints-3.6.txt
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+# The `standalone` command will 
+# - initialise the database
+# - make a user
+# - start all components for you.
+airflow standalone
+
+# Visit localhost:8080 in the browser and use the admin account details
+# shown on the terminal to login.
+# Enable the example_bash_operator dag in the home page
